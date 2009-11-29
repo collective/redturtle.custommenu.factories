92bb62a72d39aeee74bd42e4945320f6347ba367
keul <keul@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-29 17:05:10 +0000

    A lot of new stuff. Also the user UI has been started.

--- redturtle/custommenu/factories/browser/view.py
+++ redturtle/custommenu/factories/browser/view.py
@@ -1,16 +1,40 @@
 # -*- coding: utf-8 -*-
 
-from zope.interface import implements
-from Products.CMFCore.utils import getToolByName
+from zope.interface import implements, alsoProvides
+from zope.annotation.interfaces import IAnnotations
 
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from redturtle.custommenu.factories.interfaces import ICustomMenuEnabled
+
+ANN_CUSTOMMENU_KEY = 'redturtle.custommenu.factories.elements'
+
 class CustomizeFactoriesMenu(BrowserView):
-    """Form for managin custom factories menu"""
+    """View for managing custom factories menu"""
 
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
         request.set('disable_border', True)
 
-    __call__ = ViewPageTemplateFile('view.pt')
+    template = ViewPageTemplateFile('view.pt')
+
+    def addMenuEntry(self, form):
+        context = self.context
+        alsoProvides(context, ICustomMenuEnabled)
+        context.reindexObject('object_provides')
+        # now storing the data
+        annotations = IAnnotations(context)
+        
+        # BBB: to be continued
+        
+        return "{'response':'added'}"
+    
+    def __call__(self):
+        request = self.request
+        context = self.context
+        if request.form.get("action",'')=='add':
+            request.response.setHeader('Content-Type','application/json')
+            return self.addMenuEntry(request.form)
+        return self.template()
---
+++ redturtle/custommenu/factories/interfaces.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import Interface
+
+class ICustomMenuEnabled(Interface):
+    """Marker interface for content that use custom menu"""
